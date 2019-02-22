a9d2d0d2d52ffa3407224a34c1b44f64bf8021d4
Brad Goodlett <goodletb@oregonstate.edu>
2019-02-21 20:08:27 -0800

    Changed the name of the AttackStatus class. Took less than 10 seconds. Everything is still working properly and all tests pass.

--- src/main/java/cs361/battleships/models/Result.java
+++ src/main/java/cs361/battleships/models/Result.java
@@ -3,16 +3,16 @@ package cs361.battleships.models;
 public class Result {
 
 	// private members
-	private AtackStatus result;
+	private AttackStatus result;
 	private Square square;
 	private Ship ship;
 
 	// public methods
-	public AtackStatus getResult() {
+	public AttackStatus getResult() {
 		return result;
 	}
 
-	public void setResult(AtackStatus result) {
+	public void setResult(AttackStatus result) {
 		this.result = result;
 	}
 
